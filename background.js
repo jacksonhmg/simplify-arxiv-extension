67f3761e1a9df51b347b8de72a741febe839ce3c
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-28 11:25:48 -0700

    template for files

--- background.js
+++ background.js
@@ -1,11 +1,32 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.url) {
-        chrome.storage.local.get(['sites'], (result) => {
-            let sites = result.sites || [];
-            if (!sites.includes(message.url)) {
-                sites.push(message.url);
-                chrome.storage.local.set({sites: sites});
-            }
+// background.js
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.type === "simplifyContent") {
+        simplifyContent(message.title, message.description).then(simplified => {
+            chrome.tabs.sendMessage(sender.tab.id, {
+                action: 'updateContent',
+                title: simplified.title,
+                description: simplified.description
+            });
         });
     }
 });
+
+async function simplifyContent(title, description) {
+    // Call GPT (OpenAI's API) to get simplified content
+    // Make sure you have the API key and necessary setup to call the OpenAI API
+    // This is a placeholder for the actual API call
+    let simplifiedTitle = await callGPT(title);
+    let simplifiedDescription = await callGPT(description);
+
+    return {
+        title: simplifiedTitle,
+        description: simplifiedDescription
+    };
+}
+
+async function callGPT(content) {
+    // Placeholder for GPT API call
+    // You need to implement the actual API call here
+    // Return the simplified content as a string
+    return content; // This should be replaced with the actual dumbed-down version from GPT
+}
--- content.js
+++ content.js
@@ -1,2 +1,31 @@
-// Send the current page's URL to the background script
-chrome.runtime.sendMessage({type: "saveUrl", url: window.location.href});
+// content.js
+chrome.runtime.onMessage.addListener(function (message) {
+    if (message.action === 'updateContent') {
+        let metaDivs = document.querySelectorAll('.meta');
+        metaDivs.forEach(div => {
+            let titleDiv = div.querySelector('.list-title.mathjax');
+            let descriptionP = div.querySelector('p.mathjax');
+            if (titleDiv && descriptionP) {
+                titleDiv.innerHTML = message.title;
+                descriptionP.innerHTML = message.description;
+            }
+        });
+    }
+});
+
+function sendContentForSimplification() {
+    let metaDivs = document.querySelectorAll('.meta');
+    metaDivs.forEach(div => {
+        let titleDiv = div.querySelector('.list-title.mathjax');
+        let descriptionP = div.querySelector('p.mathjax');
+        if (titleDiv && descriptionP) {
+            chrome.runtime.sendMessage({
+                type: "simplifyContent",
+                title: titleDiv.innerText,
+                description: descriptionP.innerText
+            });
+        }
+    });
+}
+
+sendContentForSimplification();
